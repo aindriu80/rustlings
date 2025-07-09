101dbb224e3a4514448e7e471798690a4566cf14
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2025-07-09 15:57:30 +0100

    Completing the functions

---
+++ exercises/02_functions/functions5.rs
@@ -0,0 +1,10 @@
+// TODO: Fix the function body without changing the signature.
+fn square(num: i32) -> i32 {
+    let answer = num * num;
+    return answer;
+}
+
+fn main() {
+    let answer = square(3);
+    println!("The square of 3 is {answer}");
+}
